ae9b9879015ce3235d5fef0429926d58e469adcf
Tyler C <tcobb8@uncc.edu>
2018-11-26 20:35:45 +0000

    Created new routes for signup and created users page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root 'welcome#index'
+  get  '/Task',    to: 'tasks#show'
+  get  '/New Task',   to: 'tasks#new'
+  get  '/signup',  to: 'users#new'
+ 
   resources :tasks
   get 'welcome/index'
   
