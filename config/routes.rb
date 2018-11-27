8adcc79b7e3d0dd732751247392636defa8eb0c7
Tyler C <tcobb8@uncc.edu>
2018-11-27 22:50:53 +0000

    Added comments model and controller/routes as well as added comments to the show view.

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,9 @@ Rails.application.routes.draw do
   resources :users
   get 'welcome/index'
   
-  resources :tasks
+  resources :tasks do
+    resources :comments
+  end 
   
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_26_210218) do
+ActiveRecord::Schema.define(version: 2018_11_27_223254) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "commenter"
+    t.text "body"
+    t.integer "task_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["task_id"], name: "index_comments_on_task_id"
+  end
 
   create_table "meetings", force: :cascade do |t|
     t.string "name"
@@ -33,7 +42,6 @@ ActiveRecord::Schema.define(version: 2018_11_26_210218) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.string "time_zone"
   end
 
 end
