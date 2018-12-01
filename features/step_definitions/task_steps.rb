9da7f480286c6b7931a716885897e9660218dbe3
Taylor <tcobb10@uncc.edu>
2018-12-01 22:58:02 +0000

    First two user stories completed

--- features/step_definitions/task_steps.rb
+++ features/step_definitions/task_steps.rb
@@ -22,6 +22,3 @@ Then("I should be on the {string} page") do |page_name|
   expect(page).to have_content(page_name)
 end
 
-Then("I should select options for {string} select") do |date|
-  datetime_select(selector, :with => date)
-end
